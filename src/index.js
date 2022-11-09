76b0a5752ee77c5554cfc73d5d9a226e01edcdab
Trard <trardone@gmail.com>
2022-11-09 19:46:41 +0300

    added some plugins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { Bot } from "grammy";
+import { limit } from "@grammyjs/ratelimiter"
+import { apiThrottler } from "@grammyjs/transformer-throttler";
+import { run } from "@grammyjs/runner";
+
 import fs from "node:fs/promises";
 import { resize_dick, is_resize_time, auto_register, top } from "./actions.js";
 
 export const locale = JSON.parse(await fs.readFile("./src/locales/ru.json"));
 const bot = new Bot(process.env.TOKEN);
+
+const throttler = apiThrottler();
+bot.api.config.use(throttler);
+
+bot.use(limit());
 
 bot.on("message::bot_command", auto_register);
 
@@ -16,4 +25,4 @@ bot.command("ping", (ctx) => {
   ctx.reply(`PONG`);
 });
 
-bot.start().catch((e) => console.error(e));
+run(bot);
