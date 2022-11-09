f2ac498782e4bcb91567e15b61cf7bbe27cfcba7
Trard <trardone@gmail.com>
2022-11-09 20:20:13 +0300

    00

--- src/actions/is_resize_time.js
+++ src/actions/is_resize_time.js
@@ -2,27 +2,28 @@ import { get_user, update_last_grew_up, get_position } from "../db.js";
 import { locale } from "../index.js";
 import Mustache from "mustache";
 
-const day = 60 * 60 * 24;
+const day = 1000 * 60 * 60 * 24;
 
 export default function (ctx, next) {
-  let id = ctx.message.from.id;
+    let id = ctx.message.from.id;
 
-  let now = ctx.message.date;
-  let user = get_user(id, ctx.chat.id);
+    let now = new Date(Date.now());
+    let user = get_user(id, ctx.chat.id);
+    
+    if (now - user.last_grew_up >= day) {
+        now.setHours(0, 0, 0, 0);
+        update_last_grew_up(id, ctx.chat.id, now.getTime());
 
-  if (now - user.last_grew_up > day) {
-    //in seconds
-    update_last_grew_up(id, ctx.chat.id, now);
-    next();
-  } else {
-    ctx.reply(
-      Mustache.render(locale.not_resize_time, {
-        name: user.name,
-        id: id,
-        value: user.size,
-        current_position: get_position(id, ctx.chat.id),
-      }),
-      { parse_mode: "markdown" }
-    );
-  }
+        next();
+    } else {
+        ctx.reply(
+            Mustache.render(locale.not_resize_time, {
+                name: user.name,
+                id: id,
+                value: user.size,
+                current_position: get_position(id, ctx.chat.id),
+            }),
+            { parse_mode: "markdown" }
+        );
+    }
 }
